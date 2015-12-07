678d51ec1ca5cedf05169de020d6ff73dc780369
duerno <felipeduernoal@gmail.com>
2015-12-07 00:43:41 -0200

    Fix indentation, remove :name from user model, fix user validations, update text for destroy user, and fix edit page of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,9 @@ class UsersController < ApplicationController
 	
 	def show
 		@user = User.find(params[:id])
-
 		respond_to do |format|
-				format.html # show.html.erb
-				format.xml { render :xml => @user }
+			format.html # show.html.erb
+			format.xml { render :xml => @user }
 		end
 
 		@data = {
